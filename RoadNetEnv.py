16c99a4724cbb0569469921f68bb9580f9b9a3a0
Ujan Sen <55211866+ujansen@users.noreply.github.com>
2023-10-27 15:02:33 -0400

    Minor changes in equality checks

--- RoadNetEnv.py
+++ RoadNetEnv.py
@@ -30,7 +30,8 @@ class RoadNetEnv(gym.Env):
 
         for i in range(self.n_nodes):
             for j in range(5):
-                pygame.draw.line(canvas, (0, 0, 0), self.node_positions[i] * self.window_size, self.node_positions[self.graph[i][j]] * self.window_size)
+                pygame.draw.line(canvas, (0, 0, 0), self.node_positions[i] * self.window_size,
+                                 self.node_positions[self.graph[i][j]] * self.window_size)
 
         for i in range(self.n_nodes):
             pygame.draw.circle(
@@ -89,9 +90,9 @@ class RoadNetEnv(gym.Env):
     def step(self, action):
         self._timer += 1
         if self._timer % 10 == 0:
-            self._traffic_node_colours = {index: np.random.randint(0, 2) > 0.5 for
-                                          index in np.where(self._traffic_nodes)[0]}
+            self._traffic_node_colours = {key: 1 - value for key, value in self._traffic_node_colours.items()}
 
+        terminated = False
         cur_pos = self._agent_pos
         self._agent_pos = self.graph[self._agent_pos][action]
         reward = 0.
@@ -101,10 +102,12 @@ class RoadNetEnv(gym.Env):
         elif self._agent_pos == cur_pos and action != 4:
             print(f"Tried going from {self.node_positions[cur_pos]} to "
                   f"{self.node_positions[self._agent_pos]}")
-            reward = -5.0
+            reward = -1.0
         elif (self._traffic_nodes[cur_pos] and self._traffic_node_colours[cur_pos] == 0 and
               action != 4):
-            reward = 5 * self._find_penalty(cur_pos)
+            if np.random.rand() < self._find_accident_prob(cur_pos):
+                reward = -5.0
+                terminated = True
             print(f"Tried going from {self.node_positions[cur_pos]} to "
                   f"{self.node_positions[self._agent_pos]} while "
                   f"red traffic light")
@@ -113,7 +116,7 @@ class RoadNetEnv(gym.Env):
 
         observation = self._get_obs()
 
-        terminated = not np.any(self._target_nodes)
+        terminated = not np.any(self._target_nodes) if not terminated else terminated
 
         info = self._get_info()
 
@@ -122,7 +125,7 @@ class RoadNetEnv(gym.Env):
         self._previous_pos = cur_pos
         return observation, reward, terminated, False, info
 
-    def _find_penalty(self, cur_pos):
+    def _find_accident_prob(self, cur_pos):
         prev_node_coord = self.node_positions[self._previous_pos]
         cur_node_coord = self.node_positions[cur_pos]
         next_node_coord = self.node_positions[self._agent_pos]
@@ -130,14 +133,14 @@ class RoadNetEnv(gym.Env):
         prev_vector = cur_node_coord - prev_node_coord
         next_vector = next_node_coord - cur_node_coord
 
-        angle = np.arccos(np.clip(np.dot(prev_vector/np.linalg.norm(prev_vector),
-                                         next_vector/np.linalg.norm(next_vector)), -1.0, 1.0))
+        angle = np.arccos(np.clip(np.dot(prev_vector / np.linalg.norm(prev_vector),
+                                         next_vector / np.linalg.norm(next_vector)), -1.0, 1.0))
 
-        if 0 <= angle <= np.pi/4 or (315 * np.pi)/180 <= angle <= 2 * np.pi:
+        if 0 <= angle <= np.pi / 4 or (315 * np.pi) / 180 <= angle <= 2 * np.pi:
             direction_key = 0
-        elif np.pi/4 <= angle <= (135 * np.pi)/180:
+        elif np.pi / 4 < angle <= (135 * np.pi) / 180:
             direction_key = 1
-        elif (135 * np.pi)/180 <= angle <= (225 * np.pi)/180:
+        elif (135 * np.pi) / 180 < angle <= (225 * np.pi) / 180:
             direction_key = 2
         else:
             direction_key = 3
@@ -145,7 +148,6 @@ class RoadNetEnv(gym.Env):
         penalty = self._accident_prob[direction]
 
         return penalty
-
 
     def __init__(self, render_mode=None, graph=None, node_positions=None):
         if graph is None:
@@ -165,9 +167,9 @@ class RoadNetEnv(gym.Env):
 
         self._agent_pos = randint(0, self.n_nodes - 1)
         self._target_nodes = np.random.rand(self.n_nodes) > 0.9
-        self._traffic_nodes = np.random.rand(self.n_nodes) > 0.8
+        self._traffic_nodes = np.random.rand(self.n_nodes) > 0.6
         while np.any(np.logical_and(self._traffic_nodes, self._target_nodes)):
-            self._traffic_nodes = np.random.rand(self.n_nodes) > 0.8
+            self._traffic_nodes = np.random.rand(self.n_nodes) > 0.6
         self._traffic_node_colours = {index: np.random.randint(0, 2) > 0.5 for
                                       index in np.where(self._traffic_nodes)[0]}
         self._timer = 1
@@ -179,10 +181,10 @@ class RoadNetEnv(gym.Env):
             3: "back"
         }
         self._accident_prob = {
-            "straight": -0.9,
-            "left": -0.8,
-            "right": -0.4,
-            "back": -0.95
+            "straight": 0.9,
+            "left": 0.8,
+            "right": 0.4,
+            "back": 0.95
         }
         self.observation_space = spaces.Dict(
             {
@@ -214,6 +216,6 @@ class RoadNetEnv(gym.Env):
 
 
 register(
-     id="RoadNetEnv-v0",
-     entry_point="RoadNetEnv:RoadNetEnv",
+    id="RoadNetEnv-v0",
+    entry_point="RoadNetEnv:RoadNetEnv",
 )
